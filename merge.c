a70b190c26db7baeb8eefe72b7a1d4ade5d6d0ee
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-02-24 20:46:45 +0530

    Add files via upload

---
+++ merge.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define MAX 100000
+
+void mergeSort(int arr[],int low,int mid,int high);
+void partition(int arr[],int low,int high);
+
+int main(){
+
+    int merge[MAX],i,n;
+     double t;
+
+    printf("Enter the total number of elements: ");
+    scanf("%d",&n);
+     clock_t start,end;
+    start=clock();
+
+
+    for(i=0;i<n;i++){
+         {merge[i]=(rand()%n+1);}
+    }
+
+    partition(merge,0,n-1);
+end=clock();
+
+    t=((double)end-start)/CLOCKS_PER_SEC;
+    printf("Time is :%lf ",t);
+    /*
+    printf("After merge sorting elements are: ");
+    for(i=0;i<n;i++){
+         printf("%d ",merge[i]);
+    }*/
+
+   return 0;
+}
+
+void partition(int arr[],int low,int high){
+
+    int mid;
+
+    if(low<high){
+         mid=(low+high)/2;
+         partition(arr,low,mid);
+         partition(arr,mid+1,high);
+         mergeSort(arr,low,mid,high);
+    }
+}
+
+void mergeSort(int arr[],int low,int mid,int high){
+
+    int i,m,k,l,temp[MAX];
+
+    l=low;
+    i=low;
+    m=mid+1;
+
+    while((l<=mid)&&(m<=high)){
+
+         if(arr[l]<=arr[m]){
+             temp[i]=arr[l];
+             l++;
+         }
+         else{
+             temp[i]=arr[m];
+             m++;
+         }
+         i++;
+    }
+
+    if(l>mid){
+         for(k=m;k<=high;k++){
+             temp[i]=arr[k];
+             i++;
+         }
+    }
+    else{
+         for(k=l;k<=mid;k++){
+             temp[i]=arr[k];
+             i++;
+         }
+    }
+
+    for(k=low;k<=high;k++){
+         arr[k]=temp[k];
+    }
+}
