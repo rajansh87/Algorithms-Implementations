ccadcbf3b663168604f69a45889019714dcc304f
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-03-12 00:53:55 +0530

    Bucket Sort

--- bucket.cpp
+++ bucket.cpp
@@ -22,7 +22,6 @@ int main()
     float arr[] = {0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
     int n = sizeof(arr)/sizeof(arr[0]);
     bucketSort(arr, n);
-
     cout << "Sorted array is \n";
     for (int i=0; i<n; i++)
        cout << arr[i] << " ";
