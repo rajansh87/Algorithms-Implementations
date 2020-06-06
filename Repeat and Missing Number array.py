1b3d58079ea4823441b274a0dc99914ba01bf015
Anshraj Shrivastava <42239140+rajansh87@users.noreply.github.com>
2020-06-06 17:26:04 +0530

    Add files via upload

---
+++ Repeat and Missing Number array.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return a list of integers
+    def repeatedNumber(self, A):
+        n = len(A)
+        x = sum(A) - sum(set(A))
+        k = sum(A) - int(n*(n+1)/2)
+        
+        return [x,x-k]
